628dc797147fc0d75d0d6e9c91268468717e1c3c
Guilherme da Silveira Taschner <107483516+GuilhermeTaschner@users.noreply.github.com>
2023-02-20 22:45:04 -0300

    .

--- index.js
+++
@@ -1,8 +0,0 @@
-import processText from "./dist/cli/index.js";
-
-async function process(){
-    const result = await processText("./archive/texto.md", true)
-    console.log(result)
-}
-
-process()
